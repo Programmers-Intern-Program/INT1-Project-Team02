91b4c0a02b6c2ef390221663769ebe8b4888d64b
suyeonTech <wsy-ofc@naver.com>
2026-04-24 10:41:23 +0900

    spring init

---
+++ src/main/java/com/flodiback/FlodiBackApplication.java
@@ -0,0 +1,13 @@
+package com.flodiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlodiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlodiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flodiback/FlodiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flodiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlodiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
