dbddb6fa4fa825fb2f31b852863dabd096ff4a99
MADUPAL <kevin488@naver.com>
2026-04-28 11:45:56 +0900

    conflict 해소 후 spotless체크

--- src/main/java/com/flodiback/domain/meeting/meetinglog/repository/UtteranceRepository.java
+++ src/main/java/com/flodiback/domain/meeting/meetinglog/repository/UtteranceRepository.java
@@ -1,7 +1,9 @@
 package com.flodiback.domain.meeting.meetinglog.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.flodiback.domain.meeting.meetinglog.entity.Utterance;
 
 public interface UtteranceRepository extends JpaRepository<Utterance, Long> {
