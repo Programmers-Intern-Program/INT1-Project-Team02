a3a999c8825e32aadbe13b9e4d3eb9897cf4c2a6
zunobk <junho4560@naver.com>
2026-05-08 16:36:10 +0900

    fix: GlmClient 빈 생성자 지정

--- src/main/java/com/flodiback/global/client/GlmClient.java
+++ src/main/java/com/flodiback/global/client/GlmClient.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -56,6 +57,7 @@ public class GlmClient {
     private final Function<ChatCompletionCreateParams, ChatCompletion> completionRequester;
     private final String model;
 
+    @Autowired
     public GlmClient(
             @Value("${glm.api.key}") String apiKey,
             @Value("${glm.api.model}") String model,
