865f5e393862374abe184510c6f0b1ef1af3b02a
송찬의 <dforce103@gmail.com>
2026-05-13 23:01:29 +0900

    feat: ProjectResponse에 serverName 필드 추가 closes #109

--- src/main/java/com/flodiback/domain/project/project/dto/ProjectResponse.java
+++ src/main/java/com/flodiback/domain/project/project/dto/ProjectResponse.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record ProjectResponse(
         Long id,
         Long serverId,
+        String serverName,
         String name,
         String description,
         String techStack,
